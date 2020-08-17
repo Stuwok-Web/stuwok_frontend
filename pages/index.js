58d76edc0e6f321a9a4ac3b9e31b07cbc303b3cb
stuwok <stuwokinfo@gmail.com>
2020-08-17 14:10:45 +0100

    Project setup and created Navbar Component

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Stuwok</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Stuwok</h1>
+      </main>
+    </div>
+  )
+}
