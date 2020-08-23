179089e44b204940534bce9f258b18221626cb5a
stuwok <stuwokinfo@gmail.com>
2020-08-23 22:15:15 +0100

    created about page files

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import styles from '../styles/About.module.css'
+
+const about = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default about
